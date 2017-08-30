5df027bb94a424d9f7950723d85e3029e1ed632f
Federico Bonfiglio <fedebonfi95@gmail.com>
2017-08-30 20:17:28 +0200

    class, web developed

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template,request
+from prova import Automation
+from threading import Thread
 
 app = Flask(__name__)
+aut = Automation()
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    thread = Thread(target=aut.run()) 
+    thread.start()
+    return render_template('index.html')
+
+@app.route('/on/')
+def light_on():
+    aut.on()
+    return render_template('index.html')
+
+@app.route('/off/')
+def light_off():
+    aut.off()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
