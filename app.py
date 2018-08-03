ee701a2623f4cbf3347b283c1d543d4486f6bcf0
pi <>
2018-08-03 13:39:04 +0000

    Revert "writes a file instead of turning on and off"
    This reverts commit 554c2406c7d6f9fb7d68a80a675ed9011c6605a8.

--- app.py
+++ app.py
@@ -65,15 +65,11 @@ def run():
             output = nn.think(array([data['time'],dst,data['light']]))
             while output and not ls.is_on():
                 if light_switch_mylock.acquire():
-                    with open('switch','w') as f:
-                        f.write('on\n')
-                    #ls.activate()
+                    ls.activate()
                     light_switch_mylock.release()
             while not output and ls.is_on():
                 if light_switch_mylock.acquire():
-                    with open('switch','w') as f:
-                        f.write('off\n')
-                    #ls.activate()
+                    ls.activate()
                     light_switch_mylock.release()
         except Exception:
             pass
